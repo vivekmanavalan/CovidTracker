b48d225322b2a0b919138b03696c4c207c11fce4
vivekmanavalan <vivekmanavalan@github.com>
2020-05-01 00:35:06 +0530

    Updates

---
+++ precache-manifest.baba26bfb6a70395231270c8f064136b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9fab5445cd62e014fbe73455759962e",
+    "url": "/CovidTracker/index.html"
+  },
+  {
+    "revision": "386422079d85709851f9",
+    "url": "/CovidTracker/static/css/main.0ae94c64.chunk.css"
+  },
+  {
+    "revision": "6b0cc05e28789629cb2a",
+    "url": "/CovidTracker/static/js/2.bf3158a0.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/CovidTracker/static/js/2.bf3158a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "386422079d85709851f9",
+    "url": "/CovidTracker/static/js/main.c9206e31.chunk.js"
+  },
+  {
+    "revision": "f24a48d69bf495da1ca8",
+    "url": "/CovidTracker/static/js/runtime-main.88d0797f.js"
+  }
+]);
